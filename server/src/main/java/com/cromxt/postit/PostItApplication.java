59fa9a6a870e2dd9b0cfefda2fd8d987f30a79fd
Akash Biswas <akashbiswas.271999@gmail.com>
2025-07-31 14:24:27 +0530

    Project intialized.

---
+++ server/src/main/java/com/cromxt/postit/PostItApplication.java
@@ -0,0 +1,12 @@
+package com.cromxt.postit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostItApplication.class, args);
+	}
+}
